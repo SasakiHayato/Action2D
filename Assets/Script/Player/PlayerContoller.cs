89fc3de74a4c523acbf16ab9fa4c1094f9d39a4c
SasakiHayato0605 <zuomusundou685@gmail.com>
2021-07-05 20:16:44 +0900

    player の連続攻撃追加

--- Assets/Script/Player/PlayerContoller.cs
+++ Assets/Script/Player/PlayerContoller.cs
@@ -4,46 +4,64 @@ using UnityEngine;
 
 public class PlayerContoller : MonoBehaviour
 {
-    [SerializeField] private float speed = 0;
-    [SerializeField] private float jumpPower = 0;
+    [SerializeField] private float m_speed = 0;
+    [SerializeField] private float m_jumpPower = 0;
     
     private bool m_freeze;
-    private bool m_bool = false;
+    private bool m_active = false;
 
     [SerializeField] public int m_Hp = 0;
     [SerializeField] public int m_attackPower = 0;
 
-    [SerializeField] GroundChack groundChack;
+    private float m_time = 2;
+    private int m_num = 1;
+
+    [SerializeField] GroundChack m_groundChack;
     
     Rigidbody2D m_rigidbody;
     Animator m_animator;
 
-    GameObject attackCollider;
+    GameObject m_attack1;
+    GameObject m_attack2;
+    GameObject m_attack3;
+
     void Start()
     {
         m_rigidbody = GetComponent<Rigidbody2D>();
         m_animator = GetComponent<Animator>();
 
-        attackCollider = transform.GetChild(0).gameObject;
-        attackCollider.SetActive(m_bool);
+        m_attack1 = transform.GetChild(0).gameObject;
+        m_attack1.SetActive(m_active);
+
+        m_attack2 = transform.GetChild(1).gameObject;
+        m_attack2.SetActive(m_active);
+
+        m_attack3 = transform.GetChild(2).gameObject;
+        m_attack3.SetActive(m_active);
+
         transform.position = this.transform.position;
     }
 
     void Update()
     {
-        
-
         Move();
         if (Input.GetButtonDown("Jump"))
         {
             Jump();
         }
 
-        //Attack
         if (Input.GetButtonDown("Fire1"))
         {
-            m_animator.Play("Player_Attack");
+            m_time -= Time.deltaTime;
+
+            if (m_time < 0)
+            {
+                m_num = 1;
+                m_time = 2;
+            }
+            Attack();
         }
+        
     }
 
     void Move()
@@ -76,15 +94,36 @@ public class PlayerContoller : MonoBehaviour
             m_animator.Play("Player_Crouch");
         }
 
-        m_rigidbody.velocity = new Vector2(h * speed, m_rigidbody.velocity.y);
+        m_rigidbody.velocity = new Vector2(h * m_speed, m_rigidbody.velocity.y);
     }
 
     void Jump()
     {
-        if (groundChack.isGround == true || groundChack.plyerJumpCount > 0)
+        if (m_groundChack.isGround == true || m_groundChack.plyerJumpCount > 0)
         {
-            m_rigidbody.AddForce(transform.up * jumpPower, ForceMode2D.Impulse);
-            groundChack.plyerJumpCount--;
+            m_rigidbody.AddForce(transform.up * m_jumpPower, ForceMode2D.Impulse);
+            m_groundChack.plyerJumpCount--;
+        }
+    }
+
+    void Attack()
+    {
+        switch (m_num)
+        {
+            case 1:
+                m_animator.Play("Player_Attack");
+                m_num = 2;
+                break;
+
+            case 2:
+                m_animator.Play("Player_Attack2");
+                m_num = 3;
+                break;
+
+            case 3:
+                m_animator.Play("Player_Attack3");
+                m_num = 1;
+                break;
         }
     }
 
@@ -114,15 +153,40 @@ public class PlayerContoller : MonoBehaviour
 
     private void SetCollider()
     {
-        if (!m_bool)
+        if (!m_active)
         {
-            m_bool = true;
+            m_active = true;
         }
         else
         {
-            m_bool = false;
+            m_active = false;
         }
-        attackCollider.SetActive(m_bool);
-        
+        m_attack1.SetActive(m_active);
+    }
+
+    private void SetCollider2()
+    {
+        if (!m_active)
+        {
+            m_active = true;
+        }
+        else
+        {
+            m_active = false;
+        }
+        m_attack2.SetActive(m_active);
+    }
+
+    private void SetCollider3()
+    {
+        if (!m_active)
+        {
+            m_active = true;
+        }
+        else
+        {
+            m_active = false;
+        }
+        m_attack2.SetActive(m_active);
     }
 }
