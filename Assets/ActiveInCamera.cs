4cfbfa8f49662d3cbc5926372642d53a140163b5
SasakiHayato0605 <zuomusundou685@gmail.com>
2021-08-15 13:13:55 +0900

    ダンジョン内の軽量化

---
+++ Assets/ActiveInCamera.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActiveInCamera : MonoBehaviour
+{
+    Renderer m_renderer;
+    GameObject m_ob;
+
+    private bool m_active = false;
+
+    void Start()
+    {
+        m_renderer = GetComponent<Renderer>();
+        m_ob = transform.GetChild(0).gameObject;
+        Debug.Log(m_ob);
+    }
+
+    void Update()
+    {
+        if (m_ob == null) return;
+        if (m_renderer.isVisible)
+        {
+            if (!m_active)
+            {
+                m_active = true;
+                m_ob.SetActive(m_active);
+            }
+        }
+        else
+        {
+            if (m_active)
+            {
+                m_active = false;
+                m_ob.SetActive(m_active);
+            }
+        }
+
+    }
+}
