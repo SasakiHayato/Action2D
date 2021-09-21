c91aeaedc6578a54d407a9acf66693cd93ddaec2
SasakiHayato0605 <zuomusundou685@gmail.com>
2021-09-21 23:44:00 +0900

    Spriteを消すようにした

---
+++ Assets/Script/RemoveImage.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RemoveImage : MonoBehaviour
+{
+    SpriteRenderer m_target;
+    float r, g, b;
+    float m_alfa = 1;
+    void Start()
+    {
+        m_target = GetComponent<SpriteRenderer>();
+        r = m_target.color.r;
+        g = m_target.color.g;
+        b = m_target.color.b;
+    }
+
+    void Update()
+    {
+        m_target.color = new Color(r, g, b, m_alfa);
+        m_alfa -= Time.deltaTime / 2;
+        if (m_alfa < 0)
+            Destroy(gameObject);
+    }
+}
